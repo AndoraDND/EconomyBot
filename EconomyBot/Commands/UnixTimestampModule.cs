fa0ee7c7126742657e3f16ceaa384283bd8ebb0e
Brian Herman <Herman.brian.m@gmail.com>
2022-02-14 20:10:00 -0600

    Unix timestamp command added

---
+++ EconomyBot/Commands/UnixTimestampModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace EconomyBot.Commands
+{
+    public class UnixTimestampModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("getunix", RunMode = RunMode.Async), Alias("getut"), Summary("Get a unix timestamp from a specified time")]
+        public async Task GetUnixTimestampCommand([Remainder] string input)
+        {
+            if (DateTime.TryParse(input, out var time))
+            {
+                var value = (long)time.ToUniversalTime().Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+                await ReplyAsync($"{Context.User.Mention} Timestamp for the supplied value is : ``{value}``");
+            }
+            else
+            {
+                await ReplyAsync("Failed to get a timestamp from the specified text. Please check for any mistakes and try again.");
+            }
+
+            await Context.Channel.DeleteMessageAsync(Context.Message);
+        }
+    }
+}
